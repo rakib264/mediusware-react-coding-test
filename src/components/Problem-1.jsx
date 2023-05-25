8b5d18e600c437ea339dd5745d250404b5f736b8
Redwan Rakib <99055916+rakib264@users.noreply.github.com>
2023-05-26 00:36:36 +0600

    completed problem 1 and 2

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -1,59 +1,186 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
+import { useEffect } from "react";
 
 const Problem1 = () => {
-
     const [show, setShow] = useState('all');
+    const [name, setName] = useState('');
+    const [status, setStatus] = useState('');
+  
+    const [store, setStore] = useState([]);
+    const [items, setItems] = useState([]);
+    const [activeItems, setActiveItems] = useState([]);
+    const [completedItems, setCompletedItems] = useState([]);
 
-    const handleClick = (val) =>{
-        setShow(val);
+
+   const sortItems = (values) => {
+    const priority = {
+      active: 1,
+      completed: 2,
+    };
+
+
+    if(values && values.length){
+        const sortedItems = [...values].sort((a, b) => {
+            return priority[a.status] - priority[b.status];
+          });
+        
+    setItems(sortedItems);
     }
+  
+    
+  };
 
-    return (
-
-        <div className="container">
-            <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-1</h4>
-                <div className="col-6 ">
-                    <form className="row gy-2 gx-3 align-items-center mb-4">
-                        <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Name"/>
-                        </div>
-                        <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Status"/>
-                        </div>
-                        <div className="col-auto">
-                            <button type="submit" className="btn btn-primary">Submit</button>
-                        </div>
-                    </form>
-                </div>
-                <div className="col-8">
-                    <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
-                        <li className="nav-item">
-                            <button  className={`nav-link ${show==='all' && 'active'}`} type="button" onClick={()=>handleClick('all')}>All</button>
-                        </li>
-                        <li className="nav-item">
-                            <button className={`nav-link ${show==='active' && 'active'}`} type="button" onClick={()=>handleClick('active')}>Active</button>
-                        </li>
-                        <li className="nav-item">
-                            <button  className={`nav-link ${show==='completed' && 'active'}`} type="button" onClick={()=>handleClick('completed')}>Completed</button>
-                        </li>
-                    </ul>
-                    <div className="tab-content"></div>
-                    <table className="table table-striped ">
-                        <thead>
-                        <tr>
-                            <th scope="col">Name</th>
-                            <th scope="col">Status</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
+  const showActiveItem = () => {
+    
+    const activeItemsData = [...store].filter(
+      (item) => item.status === "active"
     );
+    setActiveItems(activeItemsData);
+  };
+
+  const showCompletedItem = () => {
+    const completedItemDaa = [...store].filter(
+      (item) => item.status === "completed"
+    );
+    setCompletedItems(completedItemDaa);
+  };
+
+  const handleClick = (val, values = null) => {
+    setShow(val);
+    if (val === "all") {
+        sortItems(values);
+    } else if (val === "active") {
+      showActiveItem();
+    } else {
+      showCompletedItem();
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const values = [...store, {
+        name: name,
+        status: status,
+      }]
+
+    setStore(values);
+    setName("");
+    setStatus("");
+    handleClick("all", values);
+  };
+
+  const renderItem = (item, index) => (
+    <tr key={index}>
+    <td>{item.name}</td>
+    <td>{item.status}</td>
+  </tr>
+  )
+
+
+
+
+  return (
+    <div className="container">
+      <div className="row justify-content-center mt-5">
+        <h4 className="text-center text-uppercase mb-5">Problem-1</h4>
+        <div className="col-6 ">
+          <form
+            className="row gy-2 gx-3 align-items-center mb-4"
+            onSubmit={handleSubmit}
+          >
+            <div className="col-auto">
+              <input
+                type="text"
+                className="form-control"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Name"
+              />
+            </div>
+            <div className="col-auto">
+              <input
+                type="text"
+                className="form-control"
+                value={status}
+                onChange={(e) => setStatus(e.target.value.toLowerCase())}
+                placeholder="Status"
+              />
+            </div>
+            <div className="col-auto">
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className="col-8">
+          <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
+            <li className="nav-item">
+              <button
+                className={`nav-link ${show === "all" && "active"}`}
+                type="button"
+                onClick={() => handleClick("all")}
+              >
+                All
+              </button>
+            </li>
+            <li className="nav-item">
+              <button
+                className={`nav-link ${show === "active" && "active"}`}
+                type="button"
+                onClick={() => handleClick("active")}
+              >
+                Active
+              </button>
+            </li>
+            <li className="nav-item">
+              <button
+                className={`nav-link ${show === "completed" && "active"}`}
+                type="button"
+                onClick={() => handleClick("completed")}
+              >
+                Completed
+              </button>
+            </li>
+          </ul>
+          <div className="tab-content"></div>
+          <table className="table table-striped ">
+            <thead>
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Status</th>
+              </tr>
+            </thead>
+            {show === "all" && (
+              <tbody>
+                {items && items.length>0 &&
+                  items.map((item, index) => (
+                    renderItem(item, index)
+                  ))}
+              </tbody>
+            )}
+            {show === "active" && (
+              <tbody>
+                {activeItems && activeItems.length>0 &&
+                  activeItems.map((item, index) => (
+                    renderItem(item, index)
+                  ))}
+              </tbody>
+            )}
+            {show === "completed" && (
+              <tbody>
+                { completedItems && completedItems.length>0 &&
+                  completedItems.map((item, index) => (
+                    renderItem(item, index)
+                  ))}
+              </tbody>
+            )}
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Problem1;
--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,21 +1,174 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import Modal from "react-modal";
 
 const Problem2 = () => {
+  const [modalAOpen, setModalAOpen] = useState(false);
+  const [modalBOpen, setModalBOpen] = useState(false);
+  const [modalCOpen, setModalCOpen] = useState(false);
+  const [contacts, setContacts] = useState([]);
+  const [modalCInfo, setModalCInfo] = useState({});
 
-    return (
+  const handleAllContacts = () => {
+    setModalAOpen(true);
+    setModalBOpen(false);
+  }
 
-        <div className="container">
-            <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
-                
-                <div className="d-flex justify-content-center gap-3">
-                <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
-                <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
-                </div>
-                
-            </div>
+  const handleUSContacts = () => {
+    setModalAOpen(false);
+    setModalBOpen(true);
+  }
+
+  const fetchContacts = () => {
+    fetch(`https://contact.mediusware.com/api/contacts/`)
+    .then((response) => response.json())
+    .then((data) => setContacts(data.results));
+  }
+
+  const searchContacts = (searchItem=null) => {
+    fetch(`https://contact.mediusware.com/api/contacts/?search=${searchItem}`)
+    .then((response) => response.json())
+    .then((data) => setContacts(data.results));
+  }
+
+  useEffect(() => {
+    fetchContacts();
+  }, []);
+
+  const handleModalAOpen = () => {
+    setModalAOpen(true);
+  };
+
+  const handleModalBOpen = () => {
+    setModalBOpen(true);
+  };
+
+  const handleModalCOpen = (event, contact) => {
+    setModalCOpen(true); 
+    setModalCInfo(contact)
+  }
+
+  const handleModalClose = () => {
+    setModalAOpen(false);
+    setModalBOpen(false);
+    setModalCOpen(false);
+  };
+
+  const handleCheckboxChange = (event) => {
+    if(event.target.checked){
+        setContacts(
+            contacts.filter((contact) => {
+              return (
+                (event.target.checked && contact.id % 2 === 0) ||
+                !event.target.checked
+              );
+            })
+          );
+    } else{
+        fetchContacts();
+    }
+  };
+
+  const handleKeyDown = event => {
+    if (event.key === 'Enter') {
+        searchContacts(event.target.value.toLowerCase())
+    }
+  };
+
+  const handleSearchChange = event => {
+    setTimeout(() => {
+        searchContacts(event.target.value.toLowerCase())
+    }, 500)
+  };
+
+
+  const handleContactClick = (contact) => {
+    setModalCOpen(true);
+    setModalContact(contact);
+  };
+
+  const [modalContact, setModalContact] = useState(null);
+
+  return (
+    <div className="container">
+      <div className="row justify-content-center mt-5">
+        <h4 className="text-center text-uppercase mb-5">Problem-2</h4>
+
+        <div className="d-flex justify-content-center gap-3">
+          <button className="btn btn-lg btn-outline-primary" type="button" onClick={handleModalAOpen}>
+            All Contacts
+          </button>
+          <button className="btn btn-lg btn-outline-warning" type="button" onClick={handleModalBOpen}>
+            US Contacts
+          </button>
         </div>
-    );
+      </div>
+      <Modal isOpen={modalAOpen} onRequestClose={handleModalClose}>
+        <h1>Contacts</h1>
+        <ul>
+          {contacts.map((contact) => (
+            <li onClick={(event) => handleModalCOpen(event, contact)}  key={contact.id}>{contact.phone}</li>
+          ))}
+        </ul>
+        <input
+          type="checkbox"
+          id="only-even"
+          checked={contacts.every((contact) => contact.id % 2 === 0)}
+          onChange={handleCheckboxChange}
+        />
+        <label htmlFor="only-even">Only even</label>
+        <input
+          type="text"
+          id="search"
+          placeholder="Search"
+          onKeyDown={handleKeyDown}
+          style={{ display: 'block' }}
+          onChange={handleSearchChange}
+        />
+       <div style={{flex: 'column'}}>
+       <button style={{backgroundColor: "#46139f", color: "#fff"}} onClick={handleAllContacts}>All Contacts</button>
+        <button style={{ backgroundColor: "#ff7f50", color: "#fff"}} onClick={handleUSContacts}>US Contacts</button>
+        <button onClick={handleModalClose}>Close</button>
+       </div>
+      </Modal>
+      <Modal isOpen={modalBOpen} onRequestClose={handleModalClose}>
+        <h1>US Contacts</h1>
+        <ul>
+          {contacts
+            .filter((contact) => contact.country.name === "United States")
+            .map((contact) => (
+              <li onClick={(contact) => handleModalCOpen(contact)} key={contact.id}>{contact.phone}</li>
+            ))}
+        </ul>
+        <input
+          type="checkbox"
+          id="only-even"
+          checked={contacts.every((contact) => contact.id % 2 === 0)}
+          onChange={handleCheckboxChange}
+        />
+        <label htmlFor="only-even">Only even</label>
+        <input
+          type="text"
+          id="search"
+          placeholder="Search"
+          onKeyUp={handleSearchChange}
+          style={{ display: 'block' }}
+        />
+        <div style={{ flex: "column"}}>
+        <button style={{backgroundColor: "#46139f", color: "#fff"}} onClick={handleAllContacts}>All Contacts</button>
+        <button style={{backgroundColor: "#ff7f50", color: "#fff"}} onClick={handleUSContacts}>US Contacts</button>
+        <button onClick={handleModalClose}>Close</button>
+        </div>
+      </Modal>
+      <Modal isOpen={modalCOpen} onRequestClose={handleModalClose}>
+        <h1>{modalCInfo?.name}</h1>
+        <ul>
+          <li>ID: {modalCInfo?.id}</li>
+          <li>Phone: {modalCInfo?.phone}</li>
+          <li>Country: {modalCInfo?.country?.name}</li>
+        </ul>
+      </Modal>
+    </div>
+  );
 };
 
 export default Problem2;
